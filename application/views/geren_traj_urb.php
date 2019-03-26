a4dc9767fdd822ba2c92cff1417a538ebe7dde68
Unknown <sandro-matheus@hotmail.com>
2019-03-26 20:44:41 -0300

    Gerenciar Trajetos Urbanos

--- application/views/geren_traj_urb.php
+++ application/views/geren_traj_urb.php
@@ -1,78 +1,74 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sandr
- * Date: 3/26/2019
- * Time: 12:35 PM
- */
 defined('BASEPATH') or exit('No direct script access allowed');
 $this->load->helper('url');
 $this->load->view("header2");
 ?>
-
 <!-- Body init -->
-<div class="container" style="background-color: #00CC00">
-    <div class="container"><h5>Gerenciar Trajetos Urbanos</h5></div>
 
-    <div class="containerFormularioRota form-group col-md-4">
-        <form>
-            <label for="TempoMédioPerc">Tempo médio de percurso:</label>
-            <input type="text" class="form-control" id="usr">
-            <label for="hrInicial">Hora Inicial:</label>
-            <input type="time" class="form-control" id="usr">
-            <label for="hrFinal">Hora Final:</label>
-            <input type="time" class="form-control" id="usr">
-            <label for="status">Status:</label>
-            <select class="form-control" id="sel1">
-                <option>Ativo</option>
-                <option>Desativado</option>
-            </select>
-            <div class="custom-file my-4">
-                <input type="file" class="custom-file-input" id="customFile">
-                <label class="custom-file-label" for="customFile">Selecione a Concessão</label>
+<div class="trajetos_exist col-md-10">
+    <div class="card">
+        <div class="card-header border-0 d-flex justify-content-between">
+            <h3 class="mb-0 justify-content-center">Rotas Urbanas</h3>
+            <div>
+            <a class="btn text-dark btn-icon-only text-dark d-flex align-center" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fa fa-plus-circle fa-2x"> </i> <h3>Criar Rota</h3>
+            </a>
             </div>
-        </form>
-
+        </div>
+        <div class="card-body ">
+            <div class="tableParaExistentes col-md-12">
+                <table class="table table-striped table-bordered table-hover align-items-center table-flush">
+                    <thead class = "thead-light">
+                    <tr>
+                        <th>Cidade</th>
+                        <th>Ponto de Partida</th>
+                        <th>Tempo Médio</th>
+                        <th>Hora Inicial</th>
+                        <th>Hora Final</th>
+                        <th>Status</th>
+                        <th>Ações</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Alegrete</td>
+                        <td>Terminal 1</td>
+                        <td>1h</td>
+                        <td>8:00H</td>
+                        <td>22:00H</td>
+                        <td>Ativo</td>
+                        <td class="text-right">
+                                <a class="btn btn-sm btn-icon-only text-dark d-flex justify-content-center" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fa fa-ellipsis-v"></i>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                    <a class="dropdown-item" href="#">Editar</a>
+                                    <a class="dropdown-item" href="#">Excluir</a>
+                                    <a class="dropdown-item" href="#">Desativar</a>
+                                    <a class="dropdown-item" href="#">Ativar</a>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            </div>
+        </div>
     </div>
-
-
-    <table class="tableGerenTrajUrb table table-striped col-md-4">
-        <thead>
-        <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Email</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-        </tr>
-        <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-            <td>mary@example.com</td>
-        </tr>
-        <tr>
-            <td>July</td>
-            <td>Dooley</td>
-            <td>july@example.com</td>
-        </tr>
-        </tbody>
-    </table>
+</div>
+<div class="row col-md-10 d-flex justify-content-end my-1">
+    <div>
+        <button type="button" class="btn btn-success ">Salvar</button>
+        <button type="button" class="btn btn-danger ">Cancelar</button>
+    </div>
 </div>
 
-<script>
-    // Add the following code if you want the name of the file appear on select
-    $(".custom-file-input").on("change", function() {
-        var fileName = $(this).val().split("\\").pop();
-        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-    });
-</script>
+
+
 
 <!-- Body end -->
 <?php
 $this->load->view("footer2.php")
 ?>
+
