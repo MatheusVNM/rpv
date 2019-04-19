d0e0a6fd3cdac448fc3dcbf01c4c52a20c7dd73d
mathiasbald <mathiasbaldissra@hotmail.com>
2019-04-19 01:25:20 -0300

    Removendo arquivos pra poder renomear eles

--- application/controllers/Error404.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Error404 extends CI_Controller {
-	public function index()
-	{
-        $this->output->set_status_header('404');
-        $this->load->view('errors/error_404'); 	}
-}
