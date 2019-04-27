5044544c1efaa66838be43dbc4f5ddc2ecb7d524
Unknown <sandro-matheus@hotmail.com>
2019-04-27 16:11:50 -0300

    testes

--- application/tests/models/rodoviaria_model_test.php
+++ application/tests/models/rodoviaria_model_test.php
@@ -37,37 +37,37 @@ class rodoviaria_model_test extends TestCase
 
     }
 
-//    public function testInsertionRodoviariaCidadeIncorreta()
-//    {
-//        $data = $this->rodoviaria->insertRodoviaria('Rodoviaria de Alegrete', 'Maximinio', 120, 'Segabinazzi', "97543-410"
-//            , 'teste@teste.com', "(55)997328105", 4,
-//            13000000);
-//        $this->assertNotEquals(true, $data['sucess']);
-//
-//    }
-//
-//    /**
-//     * Passagem de argumento com valor acima do valor esperado. O banco deve emitir emitir um erro (1).
-//     */
-//    public function testOverflowInsertionRodoviaria(){
-//        $nome = "dsfddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-//        dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-//        dddddddddddddddddddddddddddddddddddddddddd";
-//        $data = $this->rodoviaria->insertRodoviaria($nome, 'Maximinio', 120, 'Segabinazzi', "97543-410", 'teste@teste.com'
-//            , "(55)997328105", 4,
-//            1);
-//        $this->assertNotEquals(true, $data['sucess']);
-//
-//    }
-//    /**
-//     * Passagem de argumento com tipo diferente do esperado. O banco deve emitir erro (1).
-//     *
-//     */
-//    public function testUnexpectedTypeInsertionRodoviaria(){
-//        $data = $this->rodoviaria->insertRodoviaria('Rodoviaria de Rio Grande', 'Maximinio', "AQUI DEVERIA HAVER UM NÚME
-//        RO", 'Segabinazzi', "97543-410", 'teste@teste.com', "(55)997328105", 4,
-//            1);
-//        $this->assertNotEquals(true, $data['sucess']);
-//
-//    }
+    public function testInsertionRodoviariaCidadeIncorreta()
+    {
+        $data = $this->rodoviaria->insertRodoviaria('Rodoviaria de Alegrete', 'Maximinio', 120, 'Segabinazzi', "97543-410"
+            , 'teste@teste.com', "(55)997328105", 4,
+            13000000);
+        $this->assertFalse($data['sucess']);
+
+    }
+
+    /**
+     * Passagem de argumento com valor acima do valor esperado. O banco deve emitir emitir um erro (1).
+     */
+    public function testOverflowInsertionRodoviaria(){
+        $nome = "dsfddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+        dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+        dddddddddddddddddddddddddddddddddddddddddd";
+        $data = $this->rodoviaria->insertRodoviaria($nome, 'Maximinio', 120, 'Segabinazzi', "97543-410", 'teste@teste.com'
+            , "(55)997328105", 4,
+            1);
+        $this->assertFalse(true, $data['sucess']);
+
+    }
+    /**
+     * Passagem de argumento com tipo diferente do esperado. O banco deve emitir erro (1).
+     *
+     */
+    public function testUnexpectedTypeInsertionRodoviaria(){
+        $data = $this->rodoviaria->insertRodoviaria('Rodoviaria de Rio Grande', 'Maximinio', "AQUI DEVERIA HAVER UM NÚME
+        RO", 'Segabinazzi', "97543-410", 'teste@teste.com', "(55)997328105", 4,
+            1);
+        $this->assertFalse($data['sucess']);
+
+    }
 } 
