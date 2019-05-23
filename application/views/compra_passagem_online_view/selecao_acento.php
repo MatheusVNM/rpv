7871d712de005501679eb2f811da76efed8e0b6e
mathias <mathiasbaldissera@gmail.com>
2019-05-23 18:18:40 -0300

    tentativa cartao

--- application/views/compra_passagem_online_view/selecao_acento.php
+++ application/views/compra_passagem_online_view/selecao_acento.php
@@ -4,6 +4,13 @@ $this->load->helper('url');
 $this->load->view("header2");
 ?>
 
+<head>
+    <link rel="stylesheet" href="<?= base_url('assets/styles/skeuocard.reset.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('assets/styles/skeuocard.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('assets/styles/demo.css') ?>">
+    <script src="<?= base_url('assets/js/vendor/cssua.min.js') ?>"></script>
+</head>
+
 <style>
     ol {
         list-style: none;
@@ -187,6 +194,35 @@ $numPoltronas = 50;
                 <label> Preço por cadeira:
                 </label> <input type="text" id="totalValue" class="form-control" disabled value=" R$92,27" />
             </div>
+
+            <div class="credit-card-input no-js" id="skeuocard">
+                <p class="no-support-warning">
+                    Either you have Javascript disabled, or you're using an unsupported browser, amigo! That's why you're seeing this old-school credit card input form instead of a fancy new Skeuocard. On the other hand, at least you know it gracefully degrades...
+                </p>
+                <label for="cc_type">Card Type</label>
+                <select name="cc_type">
+                    <option value="">...</option>
+                    <option value="visa">Visa</option>
+                    <option value="discover">Discover</option>
+                    <option value="mastercard">MasterCard</option>
+                    <option value="maestro">Maestro</option>
+                    <option value="jcb">JCB</option>
+                    <option value="unionpay">China UnionPay</option>
+                    <option value="amex">American Express</option>
+                    <option value="dinersclubintl">Diners Club</option>
+                </select>
+                <label for="cc_number">Card Number</label>
+                <input type="text" name="cc_number" id="cc_number" placeholder="XXXX XXXX XXXX XXXX" maxlength="19" size="19">
+                <label for="cc_exp_month">Expiration Month</label>
+                <input type="text" name="cc_exp_month" id="cc_exp_month" placeholder="00">
+                <label for="cc_exp_year">Expiration Year</label>
+                <input type="text" name="cc_exp_year" id="cc_exp_year" placeholder="00">
+                <label for="cc_name">Cardholder's Name</label>
+                <input type="text" name="cc_name" id="cc_name" placeholder="John Doe">
+                <label for="cc_cvc">Card Validation Code</label>
+                <input type="text" name="cc_cvc" id="cc_cvc" placeholder="123" maxlength="3" size="3">
+            </div>
+
             <div class="form-row mt-2 float-right">
                 <button class="btn btn-success" id="finishCompra"> Finalizar Compra</button>
             </div>
