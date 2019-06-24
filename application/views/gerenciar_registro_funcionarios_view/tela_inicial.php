dcc0cd87113ffd2aefd1ef08e270e111a9aade6e
Matheus Marques <sandro-matheus@hotmail.com>
2019-06-24 19:08:51 -0300

    Começo da tela inicial

--- application/views/gerenciar_registro_funcionarios_view/tela_inicial.php
+++ application/views/gerenciar_registro_funcionarios_view/tela_inicial.php
@@ -1,1 +1,15 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
+$this->load->helper('url');
+$this->load->view("header2");
+?>
+
+<div class="d-block">
+    <a href="<?= base_url('dashboard') ?>" class="btn btn-hover text-light btn-primary position-absolute">
+        <i class="fa fa-arrow-circle-left"></i>
+    </a>
+    <h2 class="text-center ">Gerenciar registro de funcionários</h2>
+</div>
+<?= $this->session->flashdata('error'); ?>
+<?= $this->session->flashdata('success'); ?>
+
